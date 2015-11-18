8b5ea734862e6a58246c44fadba6f0e5c9b4b27c
Jonas Nyrup <jnyrup@gmail.com>
2015-11-18 18:01:16 +0100

    more stats, code refactoring

--- icm_get_total_time.py
+++ icm_get_total_time.py
@@ -4,47 +4,105 @@
 	on OMDb API.
 	The first row is expected to be headers"""
 import sys, os, csv, requests
+from bs4 import BeautifulSoup
+
+class Stats:
+	skipped = 0
+	runtimes = []
+
+	def print(self):
+		self.runtimes.sort()
+		min_value = self.runtimes[0]
+		max_value = self.runtimes[-1]
+		median_value = median(self.runtimes)
+		total = sum(self.runtimes)
+		counted = len(self.runtimes)
+		avg_value = total / counted
+
+		hours, minutes = divmod(total, 60)
+		days, hours = divmod(hours, 24)
+
+		print("%d days, %d hours and %d minutes" % (days, hours, minutes))
+		print("%d counted, %d skipped, %d in total" % (counted, self.skipped, (counted + self.skipped)))
+		print("min: %d, max: %d, median: %d, avg %d" % (min_value, max_value, median_value, avg_value))
+
+	def add(self, time):
+		self.runtimes.append(time)
+
+	def skip(self):
+		self.skipped += 1
+
+def get_from_imdb(session, imdb_url):
+	response = session.get(imdb_url, stream=True)
+	for line in response.iter_lines():
+		if str(line).endswith(' min\''):
+			return int(str(line).split()[1])
+	return False
+
+def median(lst):
+	lstLen = len(lst)
+	index = (lstLen - 1) // 2
+
+	if (lstLen % 2):
+		return lst[index]
+	else:
+		return (lst[index] + lst[index + 1])/2.0
+
+def getTime(session, csv_file):
+	csv_reader = csv.reader(csv_file)
+	stats = Stats()
+	
+	# skip header and get index of column imdburl
+	imdb_url_index = next(csv_reader).index('imdburl')
+	for row in csv_reader:
+		imdb_url = row[imdb_url_index]
+		imdb_id = imdb_url[-10:-1]
+		response = session.get("http://www.omdbapi.com/?plot=short&i=" + imdb_id)
+
+		if response.status_code != 200:
+			print("connection problems, try later")
+			sys.exit()
+
+		info = response.json()
+		if info['Response'] != 'True': 
+			print(info)
+			stats.skip()
+			continue
+
+		title = info['Title']
+		media_type = info['Type']
+		if media_type != 'movie':
+			print('"%s" (%s) is of type "%s", skipping' % (title, imdb_id, media_type))
+			stats.skip()
+			continue
+
+		try:
+			time = int(info['Runtime'][:-4])
+		except ValueError:
+			time = get_from_imdb(session, imdb_url)
+			if time == False:
+				print('"%s" (%s) has no info on omdbapi/IMDb' % (title, imdb_id))
+				stats.skip()
+				continue
+
+		stats.add(time)
+
+	stats.print()
 
 def main():
-	args = sys.argv
-	if len(args) != 2:
-		print("1 argument expected %d given" % len(args))
-		sys.exit()
-	input_file = args[1]
-	if not os.path.isfile(input_file):
-		print("%s is not a file (did you mispell something?)" % input_file)
-		sys.exit()
-
-	with open(input_file, encoding='latin-1') as csvfile, requests.Session() as session:
-		csv_reader = csv.reader(csvfile)
-		total_time = 0
-		# skip header and get index of column imdburl
-		imdb_url_index = next(csv_reader).index('imdburl')
-		for row in csv_reader:
-			imdb_url = row[imdb_url_index]
-			imdb_id = imdb_url[-10:-1]
-			response = session.get("http://www.omdbapi.com/?plot=short&i=" + imdb_id)
-			if response.status_code != 200:
-				print("connection problems, try later")
+	args = sys.argv[1:]
+	with requests.Session() as session:
+		if len(args) == 1:
+			#argument is local file
+			input_file = args[0]
+			if not os.path.isfile(input_file):
+				print("%s is not a file (did you mispell something?)" % input_file)
 				sys.exit()
-			info = response.json()
-			if info['Response'] == 'True':
-				try:
-					total_time += int(info['Runtime'][:-4])
-				except ValueError:
-					response = session.get(imdb_url, stream=True)
-					found = False
-					for line in response.iter_lines():
-						if str(line).endswith(' min\''):
-							found = True
-							total_time += int(str(line).split()[1])
-							break
-					if not found:
-						print('"%s" (%s) has no info on omdbapi/IMDb' % (info['Title'], imdb_id))
-			else: print(info)
-		hours, minutes = divmod(total_time, 60)
-		days, hours = divmod(hours, 24)
-		print("%d days, %d hours and %d minutes" % (days, hours, minutes))
+			with open(input_file, encoding='latin-1') as csv_file:
+				getTime(session, csv_file)
+		else:
+			print("1 argument expected %d given" % len(args))
+			sys.exit()
 
 if __name__ == "__main__":
 	main()
